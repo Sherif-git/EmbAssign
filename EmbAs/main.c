5b8183ff782893a3a531dc09785f47f4646f9865
Sherif Nabel <sherifnabel123@gmail.com>
2021-04-18 11:55:09 +0200

    first student commit

---
+++ EmbAs/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student {
+    char name[20];
+    int id;
+    int day,month,year;
+    int finalscore;
+};
+
+int main()
+{
+    printf("Welcome to our project\n");
+    return 0;
+}
